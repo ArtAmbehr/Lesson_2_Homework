4c146754942663f026194cc33a839edceda1d00c
Art <art-75@yandex.ru>
2022-10-20 23:01:42 +0300

    Task_2

---
+++ Task_2/Program.cs
@@ -0,0 +1,14 @@
+﻿// 2.	По двум заданным числам проверять является ли одно квадратом другого
+Console.Write("Введите первое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+if (numberB * numberB == numberA || numberA * numberA == numberB)
+{
+   Console.Write("Да"); 
+}
+else
+{
+   Console.Write("Нет"); 
+}
+
